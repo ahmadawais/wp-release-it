db6a8fbfd06447c1199ca206ee69f8c31ac088a7
Ahmad Awais <me@AhmadAwais.com>
2020-03-12 01:30:20 +0500

    📦 NEW: Init

---
+++ utils/init.js
@@ -0,0 +1,17 @@
+const welcome = require('cli-welcome');
+
+module.exports = () => {
+	welcome(
+		`wp-release-it`,
+		`by Awais.dev\n${dim(
+			`Stargaze the repo for updates ↓\nhttps://github.com/ahmadawais/wp-release-it`
+		)}`,
+		{
+			bgColor: `#d54e21`,
+			color: `#FFFFFF`,
+			bold: true,
+			clear: true,
+			version: `v${pkgJSON.version}`
+		}
+	);
+};
